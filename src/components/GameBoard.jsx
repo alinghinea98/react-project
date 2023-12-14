484ea084463f0018c8d429e8827717c3c0f4c707
ghineaal <alin.ghinea@kronsoft.ro>
2023-12-14 17:39:00 +0200

    81 lifting state up

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ]
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        // we are not updating the previous state because if it's an object, we will change its reference
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedBoard;
+        });
+        onSelectSquare();
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, index) => <li key={index}>
+            {gameBoard.map((row, index) => <li key={index}>
                 <ol>
-                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button>{playerSymbol}</button></li>)}
+                    {row.map((playerSymbol, colIndex) => <li key={colIndex}><button onClick={() => handleSelectSquare(index, colIndex)}>{playerSymbol}</button></li>)}
                 </ol>
             </li>)}
         </ol>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Player({initialName, symbol}) {
+export default function Player({initialName, symbol, isActive}) {
     const [playerName, setPlayerName] = useState(initialName ?? '');
     const [isEditing, setIsEditing] = useState(false);
     // when you update the state based on the previous state YOU SHOULD PASS A FUNCTION to update the state immediately
@@ -11,7 +11,7 @@ export default function Player({initialName, symbol}) {
         setPlayerName(event.target.value);
     }
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
                 {isEditing ? (<input placeholder="Insert name" required onChange={handleChange} value={playerName}/>) : (<span className="player-name">{playerName}</span>)}
                 <span className="player-symbol">{symbol}</span>
